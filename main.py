37d385f4a0fe872408ca96c46c1ad47e3e5c7a00
David Bakalov <davidbakalov33@gmail.com>
2022-12-06 22:28:01 +0200

    Task 1 and 2 almost work (problem with medal counting)

--- main.py
+++ main.py
@@ -1,45 +1,79 @@
-Choice=input("Choose option")
-counter=0
-Rcountries=[]
-RYears=[]
-with open("athlete_events.csv", "r") as file:
-    file.readline()
+import sys
+
+Rcountries = []
+RYears = []
 
 
-    Country=input("input country")
-    Year=input("Input Year")
+def medals():
+    counter = 0
+    medalCount = 0
+    country = sys.argv[2]
+    year = sys.argv[3]
     while True:
-        line1=file.readline()
+        line1 = file.readline()
         if not line1:
             break
-        splitLine1=line1.split(",")
+        splitLine1 = line1.split('\t')
+
+        if splitLine1[6] == country and splitLine1[14] != "NA":
+            medalCount += 1
         Rcountries.append(splitLine1[6])
         RYears.append(splitLine1[9])
 
     file.seek(0)
     file.readline()
-    if Year in RYears and Country in Rcountries:
+    if year in RYears and country in Rcountries:
         pass
     else:
         print("wrong")
         quit()
     while True:
-        line=file.readline()
+        line = file.readline()
         if not line:
             break
 
-        splitLine=line.split(",")
+        splitLine = line.split('\t')
 
-        if Choice=="-medals":
-            if splitLine[6]==Country and splitLine[9]==Year and counter<10:
-                name=splitLine[1]
-                medal=splitLine[14]
+        if sys.argv[1] == "-medals":
+            if splitLine[6] == sys.argv[2] and splitLine[9] == sys.argv[3] and counter < 10:
+                name = splitLine[1]
+                medal = splitLine[14]
 
-                sport=splitLine[12]
-                print("name:"+name)
-                print("medal:"+medal)
-                print("sport:"+sport)
+                sport = splitLine[12]
+                print("name:" + name)
+                print("medal:" + medal)
+                print("sport:" + sport)
                 print("--------------")
-                counter+=1
+                counter += 1
+
+    print("Country has" + str(medalCount))
 
 
+def total():
+    yearTotal = sys.argv[2]
+    print(yearTotal)
+    while True:
+        lineTotal = file.readline()
+        if not lineTotal:
+            break
+        splitLineTotal = lineTotal.split('\t')
+        if splitLineTotal[14] != "NA" and yearTotal == splitLineTotal[9]:
+            name = splitLineTotal[6]
+            medal = splitLineTotal[14]
+            print("name:" + name)
+            print("medal:" + medal)
+            print("--------------")
+
+
+
+choice = sys.argv[1]
+with open("data.tsv", "r") as file:
+    file.readline()
+    if choice == "-medals":
+        medals()
+    elif choice == "-total":
+        total()
+    elif choice == "":
+        pass
+    elif choice == "":
+        pass
